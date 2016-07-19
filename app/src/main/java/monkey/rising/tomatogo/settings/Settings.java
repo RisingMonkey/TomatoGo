4baff48bf217f168e3265addb3975dda548c4c0c
godhong <1753259867@qq.com>
2016-07-19 12:36:37 +0800

    settings

--- app/src/main/java/monkey/rising/tomatogo/settings/Settings.java
+++ app/src/main/java/monkey/rising/tomatogo/settings/Settings.java
@@ -1,36 +1,47 @@
 package monkey.rising.tomatogo.settings;
 
 import android.content.Intent;
+import android.graphics.Paint;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
+
+import com.google.android.gms.appindexing.Action;
+import com.google.android.gms.appindexing.AppIndex;
+import com.google.android.gms.common.api.GoogleApiClient;
 
 import  monkey.rising.tomatogo.R;
 
 public class Settings extends AppCompatActivity {
-private Button skin;
-    private  Button label;
+    private Button skin;
+    private Button label;
+
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.settings);
-        skin = (Button)findViewById(R.id.skin);
-        label =(Button)findViewById(R.id.label);
+        skin = (Button) findViewById(R.id.skin);
+        label = (Button) findViewById(R.id.label);
         skin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent =new Intent(Settings.this, monkey.rising.tomatogo.settings.Skin.class);
+                Intent intent = new Intent(Settings.this, Skin.class);
                 startActivity(intent);
             }
         });
-        label.setOnClickListener(new View.OnClickListener()
-        {
+        label.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view){
-                Intent intent = new Intent(Settings.this,Label.class);
+            public void onClick(View view) {
+                Intent intent = new Intent(Settings.this, Label.class);
                 startActivity(intent);
+
             }
         });
-    }
-}
+
+    }}
+
