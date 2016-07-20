12367e60ace1e97c42e081eef3c82b53932afe46
TwinterCh <twinterchan@gmail.com>
2016-07-20 16:47:39 +0800

    something about settings

---
+++ app/src/main/java/monkey/rising/tomatogo/config/AchievementView.java
@@ -0,0 +1,15 @@
+package monkey.rising.tomatogo.config;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import monkey.rising.tomatogo.R;
+
+public class AchievementView extends AppCompatActivity{
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_achievement_view);
+
+    }
+}
