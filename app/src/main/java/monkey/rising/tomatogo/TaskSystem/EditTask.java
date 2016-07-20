2b25bf40748de379def76fa5c8ac0d1006002a4b
细抽筋的 <1448319514@qq.com>
2016-07-20 16:51:51 +0800

    home_activity

--- app/src/main/java/monkey/rising/tomatogo/TaskSystem/EditTask.java
+++ app/src/main/java/monkey/rising/tomatogo/TaskSystem/EditTask.java
@@ -2,6 +2,8 @@ package monkey.rising.tomatogo.TaskSystem;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
@@ -11,12 +13,12 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Spinner;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import monkey.rising.tomatogo.R;
 import monkey.rising.tomatogo.dataoperate.Task;
 import monkey.rising.tomatogo.dataoperate.TaskControl;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class EditTask extends AppCompatActivity {
    EditText content;
@@ -98,7 +100,14 @@ public class EditTask extends AppCompatActivity {
             }
         });
 
-
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
     }
 
 }
